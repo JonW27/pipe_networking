c91542b8f80ebf3140dcd593b2b9845fde7dd593
Jonw27 <18wongj@gmail.com>
2017-12-12 10:55:57 -0500

    initial classwork

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,12 +5,17 @@
   server_handshake
   args: int * to_client
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_client to the file descriptor to the downstream pipe.
 
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  mkfifo("server", 600);
+  int fd = open("server", O_RDONLY, 644);
+  close(fd[WRITE]);
+  
+  
   return 0;
 }
 
@@ -19,11 +24,13 @@ int server_handshake(int *to_client) {
   client_handshake
   args: int * to_server
 
-  Perofrms the client side pipe 3 way handshake.
+  Performs the client side pipe 3 way handshake.
   Sets *to_server to the file descriptor for the upstream pipe.
 
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  mkfifo("client", 600);
+  
   return 0;
 }
